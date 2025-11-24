57910e1838bcd3b360c1e0d2f28e22943eca1c05
Matheus José <matheusjosclima@outlook.com>
2025-11-23 23:42:50 -0300

    feat: luta pela vida

--- manutencao-equipamentos/src/app/services/orcamentoService.ts
+++ manutencao-equipamentos/src/app/services/orcamentoService.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Orcamento } from '../models/OrcamentoModel';
+import { Orcamento } from '../models/orcamentoModel';
 import { OrcamentoRequest, OrcamentoResponse } from '../models/orcamentoRequestModel';
 
 @Injectable({
